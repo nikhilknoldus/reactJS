e5264c8df1a8ef7b4c5f6adc7399d39fe0febf3e
Nikhil Kumar <nikhil.kumar@moduscreate.com>
2018-11-19 19:47:51 +0530

    Added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import BookList from "./components/book-list";
 
 class App extends Component {
     render() {
-        return <div className="App">hello</div>;
+        return (
+            <div className="App">
+                Books List
+                <BookList />
+            </div>
+        );
     }
 }
 
---
+++ client/src/components/book-list.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <ul>
+                    <li> Book Name</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
